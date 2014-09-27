8ceeee40b45c649808ba1a2226ae62bde853858d
vagrant <vagrant@rails-dev-box>
2014-09-27 20:04:29 +0000

    Mocking rspec and tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,8 +23,11 @@ class PostsController < ApplicationController
     @post.topic = @topic
     authorize @post
     if @post.save
+      @post.create_vote
+      flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
+      flash[:notice] = "Error saving post."
       redirect_to [@topic, @post]
     end
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -21,10 +21,9 @@ class Post < ActiveRecord::Base
 
    validates :title, length: { minimum: 5 }, presence: true
    validates :body, length: { minimum: 20 }, presence: true
-   # validates :topic, presence: true
-   # validates :user, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
 
-   after_create :create_vote
 
    def update_rank
      age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
@@ -33,7 +32,7 @@ class Post < ActiveRecord::Base
      update_attribute(:rank, new_rank)
    end
 
-   private
+  
 
    def create_vote
     user.votes.create(value: 1, post: self)
